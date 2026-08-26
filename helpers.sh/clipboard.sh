a73bb59349405420b8c77d6fc7b46527de3ef9df
rem <s50607@proton.me>
2026-08-26 11:17:26 +0000

    Fix helper scripts

--- helpers.sh/clipboard.sh
+++ helpers.sh/clipboard.sh
@@ -1,31 +1,34 @@
-#!/bin/bash
+#!/bin/sh
 
 # Read all incoming data from stdin into a variable
 data=$(cat)
 
 # Check if data was actually received
-if [[ -z "$data" ]]; then
+if [ -z "$data" ]; then
     echo "Error: No data received from pipe." >&2
     exit 1
 fi
 
 # Create ~/.cache/clipman-nostore.tmp to prevent clipman from putting it in history.
-touch ~/.cache/clipman-nostore.tmp
+touch /tmp/clipman-nostore.tmp
 
 # Copy the data to the Wayland clipboard
 printf '%s' "$data" | wl-copy
-
-# Remove the temporary file
-rm ~/.cache/clipman-nostore.tmp
 
 echo "Copied to clipboard and will be cleared after 10 seconds."
 
 # Spawn a background process to clear the clipboard after 10 seconds
 #
 (
-    sleep 10
+    sleep 0.5 # wl-copy starts a sub process asyncly. We need to wait it to complete some time before removing the lock file
+
+	# Remove the temporary file
+	rm -f /tmp/clipman-nostore.tmp
+
+	sleep 9.5
+
     # Only clear if the current clipboard content is still what we put in, preventing from accidentally wiping newer clipboard entries.
-    if [[ $(wl-paste) == "$data" ]]; then
+    if [ "$(wl-paste)" = "$data" ]; then
         wl-copy --clear
     fi
 ) </dev/null >/dev/null 2>&1 &
--- helpers.sh/clipman.sh
+++ helpers.sh/clipman.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Store the entry only if ~/.cache/clipman-nostore.tmp file does not exist, which is created by clipboard.sh on copies.
-if [[ ! -f "~/.cache/clipman-nostore.tmp" ]]; then
+if [ ! -f "/tmp/clipman-nostore.tmp" ]; then
 	clipman store --histpath="~/.cache/clipman-primary.json"
 fi
